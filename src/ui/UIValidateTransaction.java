f3d2e85d71a1819c695e98185654cd9fd3dc0798
Facundo Perez <facup200718@gmail.com>
2020-09-11 21:00:02 -0300

    Detallito en UIValidateTransaction

--- src/ui/UIValidateTransaction.java
+++ src/ui/UIValidateTransaction.java
@@ -10,11 +10,11 @@ public class UIValidateTransaction {
         System.out.println("Type in the transaction amount: ");
         float monto = sc.nextFloat();
 
-        if (monto >= 1000 || monto < 0) {
-            System.out.println("Invalid transaction");
-            System.out.println();
-        } else if (monto > 0 && monto < 1000) {
+        if (monto > 0 && monto < 1000) {
             System.out.println("Valid transaction");
+            System.out.println();
+        } else {
+            System.out.println("Invalid transaction");
             System.out.println();
         }
     }
